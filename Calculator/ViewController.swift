0228d2e85d9d32d00477cb3dc0ba1cdb643d8f83
Junnor <dengquanzhu@gmail.com>
2016-03-03 15:48:16 +0800

    Completing the Calculator Demo, Adding some cool auto layout
    完成了第二节课的内容。
    使用stack完成自动布局

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
-    var middleTyping: Bool = false
+    var middleTyping = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -24,6 +24,59 @@ class ViewController: UIViewController {
             middleTyping = true
         }
     }
+    
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if middleTyping {
+            enter()
+        }
+        
+        switch operation {
+           
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0)}
+            
+        default: break
+
+        }
+    }
+    
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    
+    var operandStack = [Double]()
+    @IBAction func enter() {
+        middleTyping = false
+        operandStack.append(displayValue)
+        print("operation stack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        } set {
+            display.text = "\(newValue)"
+            middleTyping = false
+        }
+    }
+    
 
 }
 
