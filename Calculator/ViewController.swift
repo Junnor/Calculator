439205f6518d91e0efab574541fa9ad9e676fa11
Junnor <dengquanzhu@gmail.com>
2016-03-04 16:22:33 +0800

    完成作业一的要求项
    1 ： 第三小题的 sin() 和 cos() 函数 计算不正确，不确定是否需要把数值转换为角度数才ok。
    2 ： Autolayout 的布局在 Portrait 的角度蛮好看，但是当 Landscape
    的时候就显得比例比较小，可能用横纵比设置会更好。

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,40 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var history: UILabel!
     @IBOutlet weak var display: UILabel!
     
     var middleTyping = false
+    var isResult = false
+    
+    var numberOfPoint = 0
+    var operation: String?
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         
-        if middleTyping {
-            display.text = display.text! + digit
+        if digit == "." {
+            ++numberOfPoint
+        }
+
+        if numberOfPoint <= 1 {
+            if middleTyping {
+                display.text = display.text! + digit
+            } else {
+                display.text = digit
+                middleTyping = true
+            }
         } else {
-            display.text = digit
-            middleTyping = true
+            --numberOfPoint
         }
     }
     
     
+    // MARK: - Operate
+    
     @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
+        operation = sender.currentTitle!
         
         if middleTyping {
             enter()
         }
         
-        switch operation {
-           
+        isResult = true
+        
+        switch operation! {
         case "×": performOperation { $0 * $1 }
         case "÷": performOperation { $1 / $0 }
         case "+": performOperation { $0 + $1 }
         case "−": performOperation { $1 - $0 }
         case "√": performOperation { sqrt($0)}
-            
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "π": performOperation { $0 * M_PI }
         default: break
-
         }
     }
     
@@ -59,13 +76,36 @@ class ViewController: UIViewController {
             enter()
         }
     }
+    
+    @IBAction func cleanEverything() {
+        history.text = nil
+        display.text = "0"
+        middleTyping = false
+        isResult = false
+        numberOfPoint = 0
+        operation = nil
+        operandStack.removeAll()
+    }
 
+    // MARK: - Text Display
     
     var operandStack = [Double]()
+    
     @IBAction func enter() {
-        middleTyping = false
         operandStack.append(displayValue)
         print("operation stack = \(operandStack)")
+        
+        if !operandStack.isEmpty && !isResult {
+            addToHistory(String(operandStack.last!))
+        }
+        
+        if !operandStack.isEmpty && operation != nil && isResult {
+            addToHistory(operation!)
+        }
+        
+        numberOfPoint = 0
+        middleTyping = false
+        isResult = false
     }
     
     var displayValue: Double {
@@ -77,6 +117,14 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    func addToHistory(str: String) {
+        if let text = history.text {
+            history.text! = text + " \(str)"
+        } else {
+            history.text = str
+        }
+    }
 
 }
 
